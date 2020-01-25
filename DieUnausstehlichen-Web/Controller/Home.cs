3569404ca892c56297de8e33b09bf96575ce2d19
technologicalMayhem <wolfshund98@gmail.com>
2020-01-25 12:47:16 +0100

    Removed leftovers from crazy, made navbar fixed, added back to top button

--- DieUnausstehlichen-Web/Controller/Home.cs
+++ DieUnausstehlichen-Web/Controller/Home.cs
@@ -11,11 +11,6 @@ namespace DieUnausstehlichen_Web.Controller
         {
             return View();
         }
-
-        public IActionResult Index2()
-        {
-            return View();
-        }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ DieUnausstehlichen-Web/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Makes the navbar scroll
+// window.onscroll = function() {myFunction()};
+//
+// function myFunction() {
+//     var navbar = document.getElementById("navbar");
+//     var sticky = navbar.offsetTop;
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 8);
+    }
+};
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        toTopButton.style.display = "block";
+    } else {
+        toTopButton.style.display = "none";
+    }
+}
