13f2f40dff9c8f0544d3261e4494f0c6ab3a4f6d
technologicalMayhem <wolfshund98@gmail.com>
2020-01-26 03:18:25 +0100

    Continued some work on the site - Added some pages - Made the navbar light up depending on the page

--- DieUnausstehlichen-Web/Controller/Home.cs
+++ DieUnausstehlichen-Web/Controller/Home.cs
@@ -11,6 +11,16 @@ namespace DieUnausstehlichen_Web.Controller
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ DieUnausstehlichen-Web/Helper/Page.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DieUnausstehlichen_Web.Helper
+{
+    public static class Page
+    {
+        public static bool IsOnPage(this ViewContext viewContext, string action, string controller)
+        {
+            return viewContext.RouteData.Values["action"].Equals(action) &&
+                   viewContext.RouteData.Values["controller"].Equals(controller);
+        }
+    }
+}
